17697461fc88cecd89f08477b3a8e70b329f2226
joaochioda <joao.chioda@dextra-sw.com>
2019-09-18 12:47:05 -0300

    connection with sql done

--- src/main/java/com/example/demo/ShopController.java
+++ src/main/java/com/example/demo/ShopController.java
@@ -1,13 +1,26 @@
 package com.example.demo;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-class ShopsController {
+public class ShopController {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public ShopController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @GetMapping("/")
     public String hello() {
-        return "Spring Boot and Google App Engine - Hello World!";
+        return "oi my love";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        String currentTimestamp = jdbcTemplate.queryForObject("SELECT CURRENT_TIMESTAMP()", String.class);
+        return "CURRENT TIMESTAMP: " + currentTimestamp;
     }
 }
